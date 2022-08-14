0dfbdd8eb0778d80dc1791fb33a3775be8d4afcc
Ammaar Aslam <ammaaraslam7@gmail.com>
2022-08-14 13:13:19 +0530

    created and organized fetch community article func and query

--- index.js
+++ index.js
@@ -2,15 +2,16 @@
 
 import init from './lib/init.js';
 import cli from './lib/cli.js';
-import getArticles from './lib/utils/getArticles.js';
+import getCommunityArticles from './lib/utils/handlers/getCommunityArticles.js';
 
 const input = cli.input;
 const flags = cli.flags;
 const { clear, debug } = flags;
 
-const articles = await getArticles();
+const communityArticles = await getCommunityArticles();
+
 (async () => {
 	input.includes(`help`) && cli.showHelp(0);
 
-	input.includes(`articles`) && console.log(articles);
+	input.includes(`articles`) && console.log(communityArticles);
 })();

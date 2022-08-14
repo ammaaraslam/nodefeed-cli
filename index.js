dc8e32f0f3eb5e60fde02a6b50d5e78d227761ae
Ammaar Aslam <ammaaraslam7@gmail.com>
2022-08-14 15:48:12 +0530

    added logic to check for flags & execute relevant funcs

--- index.js
+++ index.js
@@ -2,15 +2,34 @@
 
 import init from './lib/init.js';
 import cli from './lib/cli.js';
-import Table from 'cli-table3';
 import getCommunityArticles from './lib/utils/handlers/getCommunityArticles.js';
+import getLatestArticles from './lib/utils/handlers/getLatestArticles.js';
+import getTrendingArticles from './lib/utils/handlers/getTrendingArticles.js';
+import getArticlesByUser from './lib/utils/handlers/getArticlesByUser.js';
+import getFeaturedArticles from './lib/utils/handlers/getFeaturedArticles.js';
 
 const input = cli.input;
 const flags = cli.flags;
-const { clear, debug } = flags;
+const { latest, featured, user, trending, debug } = flags;
 
 (async () => {
 	input.includes(`help`) && cli.showHelp(0);
 
-	input.includes(`articles`) && getCommunityArticles();
+	const currentFlags = { latest, featured, user, trending };
+
+	{
+		latest && input.includes(`articles`) && getLatestArticles();
+	}
+	{
+		featured && input.includes(`articles`) && getFeaturedArticles();
+	}
+	{
+		user && input.includes(`articles`) && getArticlesByUser();
+	}
+	{
+		trending && input.includes(`articles`) && getTrendingArticles();
+	}
+	{
+		debug && console.log(cli.flags);
+	}
 })();

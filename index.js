3b78f04f1d685ba1ce3d6f0fc05cecf34f04a8a0
Ammaar Aslam <ammaaraslam7@gmail.com>
2022-08-14 09:16:41 +0530

    created function to get articles from hashnode API

--- index.js
+++ index.js
@@ -2,12 +2,15 @@
 
 import init from './lib/init.js';
 import cli from './lib/cli.js';
+import getArticles from './lib/utils/getArticles.js';
 
 const input = cli.input;
 const flags = cli.flags;
 const { clear, debug } = flags;
 
+const articles = await getArticles();
 (async () => {
-	init();
 	input.includes(`help`) && cli.showHelp(0);
+
+	input.includes(`articles`) && console.log(articles);
 })();
